262f2f4e4d53bbe7cfd4b0927f44be811435a06e
Regt <regtluningtao@gmail.com>
2025-04-01 22:18:26 +0800

    实现主程序，添加U-Net模型训练逻辑；优化utils.py中的数据集类，调整图像处理方式

--- Retinal_Vessel_Segmentation/202410311023_RegtLu/src/main.py
+++ Retinal_Vessel_Segmentation/202410311023_RegtLu/src/main.py
@@ -4,3 +4,44 @@ import torch.nn.functional as F
 import numpy as np
 import unet
 import utils
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+model = unet.UNet(1,1)
+loss_func = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+
+train_data = utils.TrainingDataset(r'Retinal_Vessel_Segmentation\202410311023_RegtLu\data_example\training')
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=1, shuffle=False)
+
+for i in range(5):
+    for batch_idx, data in enumerate(train_loader):
+        image = data[0].to('cpu')
+        target = data[1].to('cpu')
+        mask = data[2].squeeze().numpy().astype(np.float32)
+        image_1 = image[:, :, :292, :292]  # 左上
+        image_2 = image[:, :, :292, 292:]  # 右上
+        image_3 = image[:, :, 292:, :292]  # 左下
+        image_4 = image[:, :, 292:, 292:]  # 右下
+        predict_1 = model(image_1)
+        predict_2 = model(image_2)
+        predict_3 = model(image_3)
+        predict_4 = model(image_4)
+        batch_size, channels, _, _ = predict_1.shape
+        predict = torch.zeros((batch_size, channels, 584, 565), device=device)
+        predict[:, :, 0:292, 0:292] = predict_1
+        predict[:, :, 0:292, 273:565] = predict_2
+        predict[:, :, 292:584, 0:292] = predict_3
+        predict[:, :, 292:584, 273:565] = predict_4
+        predict[:, :, 0:292, 273:292] = (predict_1[:, :, 0:292, 273:292] + predict_2[:, :, 0:292, 0:19]) / 2
+        predict[:, :, 292:584, 273:292] = (predict_3[:, :, 0:292, 273:292] + predict_4[:, :, 0:292, 0:19]) / 2
+        predict = predict.to('cpu') 
+        predict * torch.tensor(mask > 0, dtype=torch.float32, device='cpu')
+        loss = loss_func(predict, target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(f"Batch {batch_idx}: Loss = {loss.item()}")
--- Retinal_Vessel_Segmentation/202410311023_RegtLu/src/utils.py
+++ Retinal_Vessel_Segmentation/202410311023_RegtLu/src/utils.py
@@ -1,11 +1,11 @@
 import numpy as np
+import torch
+import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms.functional as TF
 import os
 import cv2
 from PIL import Image
-
-def resize_image(image, size=(572, 572)):
-    return cv2.resize(image, size, interpolation=cv2.INTER_LINEAR)
 
 class TestDataset(Dataset):
     def __init__(self, data_path):
@@ -21,8 +21,6 @@ class TestDataset(Dataset):
         mask_file = os.path.join(self.mask_paths, self.file_list[idx].replace('.tif', '_mask.gif'))
         image = cv2.imread(image_file)
         mask = np.array(Image.open(mask_file).convert("L"))
-        image = resize_image(image)
-        mask = resize_image(mask)
         return image * np.expand_dims(mask > 0, axis=-1)
 
 class TrainingDataset(Dataset):
@@ -33,20 +31,20 @@ class TrainingDataset(Dataset):
         self.file_list = sorted([f for f in os.listdir(self.image_paths) if f.endswith('.tif')])
 
     def __len__(self):
-        return len(self.file_list)
+        return len(self.file_list)*4
 
     def __getitem__(self, idx):
-        base_name = self.file_list[idx].replace('.tif', '')
+        base_name = self.file_list[idx//4].replace('.tif', '')
         image_file = os.path.join(self.image_paths, f'{base_name}.tif')
         mask_file = os.path.join(self.mask_paths, f'{base_name}_mask.gif')
         manual_file = os.path.join(self.manual, f'{base_name.strip('_training')}_manual1.gif')
-        image = cv2.imread(image_file)
-        manual = np.array(Image.open(manual_file).convert("L"))
-        mask = np.array(Image.open(mask_file).convert("L"))
-        image = resize_image(image)
-        manual = resize_image(manual)
-        mask = resize_image(mask)
-        return image * np.expand_dims(mask > 0, axis=-1), manual * np.expand_dims(mask > 0, axis=-1)
+        image = TF.to_tensor(cv2.imread(image_file, cv2.IMREAD_GRAYSCALE))
+        manual =  TF.to_tensor(Image.open(manual_file).convert("L"))
+        mask = TF.to_tensor(Image.open(mask_file).convert("L"))
+        padding = (10, 9, 0, 0)
+        image = nn.ZeroPad2d(padding)(image)
+        mask_padding = nn.ZeroPad2d(padding)(mask)
+        return image * (mask_padding > 0), manual * (mask > 0), mask
 
 if __name__ == "__main__":
     train_dataset = TrainingDataset(r'Retinal_Vessel_Segmentation\202410311023_RegtLu\data_example\training')
@@ -54,22 +52,21 @@ if __name__ == "__main__":
     test_dataset = TestDataset(r'Retinal_Vessel_Segmentation\202410311023_RegtLu\data_example\test')
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
     for batch_idx, train_data in enumerate(train_loader):
-        image1,image2 = train_data
-        image1=image1.squeeze().numpy().astype(np.uint8)
-        image2=image2.squeeze().numpy().astype(np.uint8)
-        image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-        mean = np.mean(image1)
-        image1 = image1 - mean
-        image1 =np.clip(image1, 0, 255)
-        image1 = image1.astype(np.uint8)
-        image1=cv2.Canny(image1, 25, 50,apertureSize=3,L2gradient=True)
+        image1,image2,image3 = train_data
+        image1=image1.squeeze().numpy().astype(np.float32)
+        image2=image2.squeeze().numpy().astype(np.float32)
+        image3=image3.squeeze().numpy().astype(np.float32)
         cv2.imshow('Image', image1)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         cv2.imshow('Image', image2)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+        cv2.imshow('Image', image3)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         break
+    exit()
     for batch_idx, test_data in enumerate(test_loader):
         image=test_data.squeeze().numpy().astype(np.uint8)
         cv2.imshow('Image', image)
