1e31e2a93d0bff3b481ab6ab397ac48033ac0181
Regt <regtluningtao@gmail.com>
2025-04-02 14:21:57 +0800

    优化训练逻辑，修改损失函数为BCEWithLogitsLoss，使用Adam优化器；重构训练过程为函数并记录最佳损失; 更新结构图

--- Retinal_Vessel_Segmentation/202410311023_RegtLu/src/train.py
+++ Retinal_Vessel_Segmentation/202410311023_RegtLu/src/train.py
@@ -7,14 +7,15 @@ import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = unet.UNet(1, 1).to(device)
-loss_func = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+loss_func = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 train_data = utils.TrainingDataset(
     r"Retinal_Vessel_Segmentation\202410311023_RegtLu\data_example\training"
 )
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=1, shuffle=False)
 
-for i in range(10):
+def train():
+    best_loss = float("inf")
     for batch_idx, data in enumerate(train_loader):
         image = data[0].to(device)
         target = data[1].to(device)
@@ -22,9 +23,14 @@ for i in range(10):
         predict = model(image)[:, :, 2:-2, 11:-12]
         predict * torch.tensor(mask > 0, dtype=torch.float32, device=device)
         loss = loss_func(predict, target)
+        if loss < best_loss:
+            best_loss = loss
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        print(f"Batch {batch_idx}: Loss = {loss.item()}")
+    print(f"Best Loss = {best_loss}")
+
+for i in range(10):
+    train()
 
 torch.save(model, os.path.join(os.path.dirname(__file__), "model.pth"))
